0ba5186eb36ceb58fe36ed31eddb1a12732787df
Sonam Narula <sonamnarula2108@gmail.com>
2026-02-19 17:48:15 +0530

    Update brain.js: Hoisting & Variable Reassignment practice

--- JAVASCRIPT/brain.js
+++ JAVASCRIPT/brain.js
@@ -2,3 +2,17 @@
 var age = 32;
 console.log('Age is', age);
 
+sayHi();
+age = 45;
+console.log('Age is', age);
+
+function sayHi() {
+    console.log('Hi');
+}
+sayHi();
+sayHi(); 
+// Declare firstName, lastName, and fullName
+var firstName = 'John';
+var lastName = 'Doe';
+var fullName = firstName + ' ' + lastName;
+console.log('Full Name is', fullName);
