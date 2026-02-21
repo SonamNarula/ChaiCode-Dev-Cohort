c26fd8035b82f884fe2f918373290c61f4720dcd
Sonam Narula <sonamnarula2108@gmail.com>
2026-02-21 13:29:58 +0530

    feat: add variables and data types lesson and update README

---
+++ JAVASCRIPT/Variables and data types/1.js
@@ -0,0 +1,31 @@
+// console.log('Sonam Narula is the best!');
+// console.log('I am learning JavaScript');
+// console.log('I am loving it');
+
+// fullName = 'Sonam Narula';
+// console.log(fullName);
+
+// age = 21;
+// price = 99.99;
+// console.log(price);
+
+// x = null;
+// console.log(x);
+
+// y = undefined;
+// console.log(y);
+
+// let fullName1 = 'Tony Stark';
+// let agee = 20;
+// let pricee = 1000;
+
+// console.log(fullName1);
+
+var age = 24;
+var age = 90;
+var age = 200;
+
+console.log(age);
+
+let a = 5;
+console.log(a);
