91e966e0548c80a4f59bc2673005039c34f259fd
Sonam Narula <sonamnarula2108@gmail.com>
2026-02-23 17:07:20 +0530

    docs: integrate 02-variables.js updates and document 03-datatypes.js in the README

---
+++ JAVASCRIPT/BASICS - 2/02-variables.js
@@ -0,0 +1,35 @@
+var shipName = "The Amber";
+console.log("Ship name : ", shipName);
+
+let crewCount = 12;
+console.log("Crew count : ", crewCount);
+crewCount = 14;
+
+const captainName = "Jack Sparrow";
+console.log("Captain name : ", captainName);
+
+// captainName = "Dipesh";
+
+if(true){
+    var leakyTreasure = "Gold coins";
+}
+console.log(leakyTreasure);
+
+let shipSeed = 22;
+let _privatelog = "secret";
+let MONGODB_URI = "";
+let name = "hitesh";
+
+const treasureChest = {
+    gold: 100, 
+    rubies: 50,
+    maps: 2,
+};
+
+treasureChest.gold = 150;
+treasureChest = {gold: 50};
+
+const crewRoster = ["Alok", "Abhinav", "Tasnish"]
+crewRoster.push("viraj")
+crewRoster[0] = "Shubham";
+crewRoster = ["Someone"];
---
+++ JAVASCRIPT/BASICS - 2/03-datatypes.js
@@ -0,0 +1,14 @@
+const weaponName = "Flame Sword"
+console.log("Weapon : ", weaponName, "| type: ", typeof weaponName);
+
+const attackPower = 75;
+const attackUpgrade = 1.5;
+
+console.log(typeof attackPower);
+console.log(typeof attackUpgrade);
+
+const isLoggedJ  = true;
+let bonusEffect;
+
+let curseStatus = null;
+let weatherApiResponse = null
