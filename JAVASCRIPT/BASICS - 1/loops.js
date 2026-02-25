2b304019aa6054b0b1c972b8ee4daa69a1372bb6
Sonam Narula <sonamnarula2108@gmail.com>
2026-02-25 15:04:25 +0530

    docs: detail loop.js logic implementations in curriculum

---
+++ JAVASCRIPT/BASICS - 1/loops.js
@@ -0,0 +1,21 @@
+// for loop
+console.log("===FOR LOOP===");
+
+for (let i = 0; i <= 10; i++) {
+  console.log(i);
+}
+
+console.log("===WHILE LOOP===");
+
+let j = 0;
+while (j <= 10) {
+  console.log(j);
+  j++;
+}
+
+console.log("===DO - WHILE LOOP===");
+let k = 0;
+do {
+  console.log(k);
+  k++;
+} while (k < 5);
