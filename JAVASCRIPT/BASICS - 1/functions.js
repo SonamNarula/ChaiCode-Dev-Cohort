a4aaec30da8fe5001c96c871c0cb3ea8536d6311
Sonam Narula <sonamnarula2108@gmail.com>
2026-03-30 18:07:51 +0530

    functions under process

--- JAVASCRIPT/BASICS - 1/functions.js
+++ JAVASCRIPT/BASICS - 1/functions.js
@@ -17,3 +17,13 @@ const r = add(2,5);
 for(let i = 0; i < r; i++){
     console.log(`value of i is ${i}`);
 }
+
+function cartoon(){
+    function cartoonInsideCartoon(){
+        return 'Naruto 🔥';
+    }
+    // return the function itself so we can call it later
+    return cartoonInsideCartoon;
+}
+const anime = cartoon();
+anime() ;
