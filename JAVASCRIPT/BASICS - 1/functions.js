b5e02e926e81c5f6e4718c3357d6b71c73e8c993
Sonam Narula <sonamnarula2108@gmail.com>
2026-03-30 18:39:25 +0530

    functions under process

--- JAVASCRIPT/BASICS - 1/functions.js
+++ JAVASCRIPT/BASICS - 1/functions.js
@@ -1,32 +1,32 @@
-function sayHello(){}
-sayHello();
+// function sayHello(){}
+// sayHello();
 
-function greetUser(x,y,z){
-    console.log('Hey', x,y,z);
-    console.log(`helooo ${x}`);
-}
+// function greetUser(x,y,z){
+//     console.log('Hey', x,y,z);
+//     console.log(`helooo ${x}`);
+// }
 
-greetUser('Sonam');
+// greetUser('Sonam');
 
-function add(num1,num2){
-    console.log(`result is ${num1+num2}`)
-}
-add(2,5);
+// function add(num1,num2){
+//     console.log(`result is ${num1+num2}`)
+// }
+// add(2,5);
 
-const r = add(2,5);
-for(let i = 0; i < r; i++){
-    console.log(`value of i is ${i}`);
-}
+// const r = add(2,5);
+// for(let i = 0; i < r; i++){
+//     console.log(`value of i is ${i}`);
+// }
 
-function cartoon(){
-    function cartoonInsideCartoon(){
-        return 'Naruto 🔥';
-    }
-    // return the function itself so we can call it later
-    return cartoonInsideCartoon;
-}
-const anime = cartoon();
-anime() ;
+// function cartoon(){
+//     function cartoonInsideCartoon(){
+//         return 'Naruto 🔥';
+//     }
+//     // return the function itself so we can call it later
+//     return cartoonInsideCartoon;
+// }
+// const anime = cartoon();
+// anime() ;
 
 const fruits = ['apple', 'cheeku', true,'aadu' , 1 , ' santra'];
 
@@ -34,3 +34,13 @@ fruits.push('kiwi');
 console.log(fruits.length);
 console.log(fruits);
 console.log(fruits.includes('aadu'));
+
+// high order functions
+function meraPyaraFunction(udharkafunction){
+    return udharkafunction() + 40;
+}
+function cartoon(){
+    return 10;
+}
+
+console.log(meraPyaraFunction(cartoon));
