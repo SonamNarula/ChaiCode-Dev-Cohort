85bd661a6a4d64298389193c25e547b59031d736
Sonam Narula <sonamnarula2108@gmail.com>
2026-03-30 18:00:27 +0530

    update message

--- JAVASCRIPT/BASICS - 1/functions.js
+++ JAVASCRIPT/BASICS - 1/functions.js
@@ -2,7 +2,12 @@ function sayHello(){}
 sayHello();
 
 function greetUser(x,y,z){
-    console.log('Hey', x);
+    console.log('Hey', x,y,z);
+    console.log(`helooo ${x}`);
 }
 
 greetUser('Sonam');
+
+function add(num1,num2){
+    console.log(`result is ${num1+num2}`)
+}
