693f947a3cb14aa019c05f84d0c5c3c92ace37c5
Sonam Narula <sonamnarula2108@gmail.com>
2026-04-04 16:40:31 +0530

    Add functions drill and update README

---
+++ JAVASCRIPT/ESSENTIALS - 1/functions.js
@@ -0,0 +1,25 @@
+// console.log("hey");
+
+function dance(){
+    console.log("dance");
+    console.log("dance");
+    console.log("dance");
+    console.log("dance");
+    console.log("dance");
+    console.log("dance");
+}
+function lolo(){
+    console.log("Happy New Year");
+}
+lolo();
+
+
+let fnc = function(){
+    console.log("heyeyeyeyeyeheyeye");
+}
+
+// fat arrow function
+
+let fun = () => {
+    console.log("yoooooo");
+}
