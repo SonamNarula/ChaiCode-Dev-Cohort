6aa88927dfdcb1e6e00cbe637c112efa567c3fef
Sonam Narula <sonamnarula2108@gmail.com>
2026-04-08 20:09:34 +0530

    Expand conversion operator examples

--- JAVASCRIPT REVISION/01_basics/03-ConversionOperator.js
+++ JAVASCRIPT REVISION/01_basics/03-ConversionOperator.js
@@ -26,3 +26,26 @@ let someNumber = 33;
 let stringNumber = String(someNumber);
 console.log(typeof stringNumber);  // string
 console.log(stringNumber);         // "33"
+
+
+
+
+// ********************Operations***********************
+let value = 3
+let negValue = -value
+console.log(negValue)
+
+let str1 = "hello"
+let str2 = " sonam"
+console.log(str1+str2)
+
+console.log(true)
+console.log(+true) //1
+console.log(+"")
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter)
